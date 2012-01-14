949d4a234fe9dd99395844e5174f3348b30d13dc
Masayuki Muto <muto@timedia.co.jp>
2012-01-14 17:34:39 +0900

    MenuController

---
+++ Kondate/Controllers/MenuController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Kondate.Models;
+
+namespace Kondate.Controllers
+{
+    public class MenuController : Controller
+    {
+        private KondateContext db = new KondateContext();
+
+        //
+        // GET: /Menu/
+
+        public ViewResult Index()
+        {
+            return View(db.Menus.ToList());
+        }
+
+        //
+        // GET: /Menu/Details/5
+
+        public ViewResult Details(int id)
+        {
+            Menu menu = db.Menus.Find(id);
+            return View(menu);
+        }
+
+        //
+        // GET: /Menu/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Menu/Create
+
+        [HttpPost]
+        public ActionResult Create(Menu menu)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Menus.Add(menu);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(menu);
+        }
+
+        //
+        // GET: /Menu/Edit/5
+
+        public ActionResult Edit(int id)
+        {
+            Menu menu = db.Menus.Find(id);
+            return View(menu);
+        }
+
+        //
+        // POST: /Menu/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Menu menu)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(menu).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(menu);
+        }
+
+        //
+        // GET: /Menu/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            Menu menu = db.Menus.Find(id);
+            return View(menu);
+        }
+
+        //
+        // POST: /Menu/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Menu menu = db.Menus.Find(id);
+            db.Menus.Remove(menu);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
