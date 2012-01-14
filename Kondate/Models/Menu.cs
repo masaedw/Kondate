24da7c3a5f53f0bb2230a15226131f155b88112f
Masayuki Muto <muto@timedia.co.jp>
2012-01-14 17:15:00 +0900

    update menu columns

--- Kondate/Models/Menu.cs
+++ Kondate/Models/Menu.cs
@@ -1,19 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace Kondate.Models
 {
+	public enum MealTime
+	{
+		Breakfirst,
+		Lunch,
+		Dinner,
+	}
+
 	public class Menu
 	{
 		public int MenuId { get; set; }
 
 		[DisplayName("説明")]
+		[StringLength(140)]
 		public string Description { get; set; }
 
+		[DisplayName("内容")]
+		[StringLength(1024)]
+		public string Content { get; set; }
+
+		[DisplayName("日付")]
+		public DateTime Date { get; set; }
+
+		public MealTime MealTime { get; set; }
+
+		[DisplayName("作成日時")]
 		public DateTime CreatedAt { get; set; }
+		[DisplayName("更新日時")]
 		public DateTime UpdatedAt { get; set; }
 
 		public virtual ICollection<Meal> Meals { get; set; }
